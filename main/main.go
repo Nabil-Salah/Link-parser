a4fe71df69aeaa9bb57af88603601c77a9eb86c2
Nabil Salah <nabil.salah203@gmail.com>
2024-04-03 22:22:33 +0200

    link parser Finnish

--- main/main.go
+++ main/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"Link-parser"
+	"fmt"
+	"golang.org/x/net/html"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var htmlparsing string = `<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<h1>Social stuffs</h1>
+<div>
+    <a href="https://www.twitter.com/joncalhoun">
+        Check me out on twitter
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+    </a>
+    <a href="https://github.com/gophercises">
+        Gophercises is on <strong>Github</strong>!
+    </a>
+</div>
+</body>
+</html>`
+	a, err := link.Parse(html.NewTokenizer(strings.NewReader(htmlparsing)))
+	if err != nil {
+		fmt.Println(err)
+		//return
+	}
+	for i, j := range a {
+		fmt.Printf("%d: href: %s text: %s\n", i+1, j.Href, j.Text)
+	}
 }
